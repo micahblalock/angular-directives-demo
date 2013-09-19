0317e9546b76d5f9605c9bb892c980770b4a1552
Micah Blalock <mblalock@availity.com>
2013-09-19 13:46:04 -0500

    added simple web flow wizard

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,6 +15,37 @@ angular.module('angDemo', ['ngRoute','demo.directives','ngAnimate'])
     var user  = {};
     user.checkAmount = '13.00';
     user.checkDate = '07/30/1983';
-    return  user;
+    var getUser = function() {
+      return user;
+    }
+    var saveUser = function() {
+      console.log("User saved");
+    }
+
+    return {
+      get: getUser,
+      save: saveUser
+    }
+  })
+  .factory('Payer', function() {
+    var payer = {};
+    payer.companyName = '';
+    payer.phoneNumber = '';
+    payer.email = '';
+    payer.npiNumber = '';
+    payer.taxId = '';
+    var getpayer = function() {
+      return payer;
+    }
+    var updatePayer = function(_payer) {
+      console.log('payer: ' + _payer);
+      payer = _payer
+    }
+    return {
+      get: getpayer,
+      save: updatePayer
+    };
   });
 
+
+  
